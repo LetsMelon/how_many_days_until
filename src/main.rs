46030b729ee75284157de19effde9b286434601a
LetsMelon <domi.m@outlook.com>
2022-12-02 13:55:34 +0100

    use clap to parse args

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,36 @@
 use chrono::naive::NaiveDate;
 use chrono::offset::Local;
 use chrono::Datelike;
+use clap::{value_parser, Arg, Command, Parser, ValueHint};
 use how_many_days_until::count_days_in_between;
 
 fn main() {
-    let start = Local::now().date_naive();
-    let end = NaiveDate::from_ymd_opt(start.year(), 12, 24).unwrap();
+    let args = Command::new("how_many_days_until")
+        .author("Domenic Melcher, domi.m@outlook.com")
+        .version("0.1.0")
+        .about("Command line tool to calculate how many days are until a date, considering national holidays.")
+        .arg_required_else_help(true)
+        .args([
+            Arg::new("start")
+                .short('s')
+                .help("Start date; format: YYYY-MM-DD")
+                .required(false)
+                .value_hint(ValueHint::Other)
+                .value_parser(value_parser!(NaiveDate)),
+            Arg::new("end")
+                .short('e')
+                .help("Until when inclusive; format: YYYY-MM-DD")
+                .required(true)
+                .value_hint(ValueHint::Other)
+                .value_parser(value_parser!(NaiveDate)),
+        ])
+        .get_matches();
+
+    let start = match args.get_one::<NaiveDate>("start") {
+        Some(date) => *date,
+        None => Local::now().date_naive(),
+    };
+    let end = *args.get_one::<NaiveDate>("end").unwrap();
 
     let (total_days, working, free) = count_days_in_between(start, end);
 
